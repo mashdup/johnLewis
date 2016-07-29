1a2fd72a6b4543b92e3da565a27c6dbc8b453e5e
Dillon Hoa <mr_d_2chat@hotmail.com>
2016-07-29 10:32:03 +0100

    Initial Commit

---
+++ johnLewis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  johnLewis
+//
+//  Created by Dillon Hoa on 29/07/2016.
+//  Copyright © 2016 8bitDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ johnLewis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  johnLewis
+//
+//  Created by Dillon Hoa on 29/07/2016.
+//  Copyright © 2016 8bitDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
